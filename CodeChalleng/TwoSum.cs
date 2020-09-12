febee80561092c63c1c6fe9f1bc075eb7fb042e8
ammarAlhamoud <ammarAlhamoud@users.noreply.github.com>
2020-09-12 09:30:02 +0200

    Commit test

---
+++ CodeChalleng/TwoSum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeChalleng
+{
+    /// <summary>
+    /// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+    /// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    /// You can return the answer in any order
+    /// </summary>
+    public class TwoSum
+    {   
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public int[] GetResult(int[] nums, int target)
+        {
+            target = 3;
+            return new int[2];
+        }
+    }
+}
