83429786eb2c80320886c9d85fef76408a8c0b04
ammarAlhamoud <ammarAlhamoud@users.noreply.github.com>
2020-09-12 10:29:26 +0200

    implement function to get the result

--- CodeChalleng/AppTest.cs
+++ CodeChalleng/AppTest.cs
@@ -11,6 +11,11 @@ namespace CodeChalleng
         {
             // first test for consol output
             Console.WriteLine("Hello World!");
+
+            TwoSum twoSum = new TwoSum();
+            int[] nums = new int[] {2, 7, 11, 15, 1, 12, 15, 22 , 103, 66};
+            int target = 23;
+            twoSum.GetResult(nums, target);
         }
     }
 }
--- CodeChalleng/TwoSum.cs
+++ CodeChalleng/TwoSum.cs
@@ -12,15 +12,38 @@ namespace CodeChalleng
     public class TwoSum
     {   
         /// <summary>
-        /// 
+        /// Return two indecies of the result array
         /// </summary>
         /// <param name="nums"></param>
         /// <param name="target"></param>
         /// <returns></returns>
         public int[] GetResult(int[] nums, int target)
         {
-            target = 3;
-            return new int[2];
+            int[] result = new int[2];
+            for(int firstIndex=0; firstIndex<nums.Length; firstIndex++)
+            {
+                result[0] = firstIndex;
+                int secondIndex = firstIndex + 1;
+                if (secondIndex > nums.Length-1)
+                {
+                    Console.WriteLine("Error: Out of rang");
+                    throw new IndexOutOfRangeException();
+                }
+
+                for(;secondIndex<nums.Length; secondIndex++)
+                {
+                    int sum = nums[firstIndex] + nums[secondIndex];
+                    if (sum == target)
+                    {
+                        result[1] = secondIndex;
+                        Console.WriteLine("The Result is: [" + result[0] + ", " + result[1] + "]");
+                        return result;
+                    }
+                }
+            }
+
+            Console.WriteLine("There is no answer for the given array");
+            return null;
         }
     }
 }
