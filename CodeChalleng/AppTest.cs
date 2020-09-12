25c6d579a591d43c30f38fc99934bdb23c8edc60
ammarAlhamoud <ammarAlhamoud@users.noreply.github.com>
2020-09-12 09:02:54 +0200

    Add project files.

---
+++ CodeChalleng/AppTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CodeChalleng
+{
+    /// <summary>
+    /// The startup class for this application
+    /// </summary>
+    class AppTest
+    {
+        static void Main(string[] args)
+        {
+            // first test for consol output
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
